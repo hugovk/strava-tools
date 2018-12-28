1ab1e6cfc587369dd782abbddfac3d03ebdce44c
Hugo <hugovk@users.noreply.github.com>
2018-12-28 13:58:03 +0200

    More plots

--- stravaviz.R
+++ stravaviz.R
@@ -22,13 +22,20 @@ ggsave("plots/facets-all.png", p1, width = 20, height = 20, units = "cm")
 
 print('Plot activity map')
 
-# p2 <- plot_map(data, lon_min = 24.8312, lon_max = 25.2022, lat_min = 60.1301, lat_max = 60.2893) # Helsinki
-# p2 <- plot_map(data, lon_min = 24.4993, lon_max = 25.2022, lat_min = 60.1301, lat_max = 60.2893) # Helsinki but Espoo's lon_min
-# p2 <- plot_map(data, lon_min = 24.4993, lon_max = 25.2545, lat_min = 59.9011, lat_max = 60.3639) # Helsinki/Espoo
-# p2 <- plot_map(data, lon_min = 24.4993, lon_max = 25.2545, lat_min = 59.9011, lat_max = 60.401375) # Helsinki/Espoo/Vantaa
+p2_hki <- plot_map(data, lon_min = 24.807278, lon_max = 25.087382, lat_min = 60.144413, lat_max = 60.221064) # Helsinki centre
+p2_h <- plot_map(data, lon_min = 24.8312, lon_max = 25.2022, lat_min = 60.1301, lat_max = 60.2893) # Helsinki
+p2_h2 <- plot_map(data, lon_min = 24.4993, lon_max = 25.2022, lat_min = 60.1301, lat_max = 60.2893) # Helsinki but Espoo's lon_min
+p2_he <- plot_map(data, lon_min = 24.4993, lon_max = 25.2545, lat_min = 59.9011, lat_max = 60.3639) # Helsinki/Espoo
+p2_hev <- plot_map(data, lon_min = 24.4993, lon_max = 25.2545, lat_min = 59.9011, lat_max = 60.401375) # Helsinki/Espoo/Vantaa
 p2 <- plot_map(data, lon_min = 22.65, lon_max = 26.65, lat_min = 59.61, lat_max = 60.84) # Uusimaa
 
-ggsave("plots/map-all.png", p2, width = 20, height = 15, units = "cm", dpi = 600)
+ggsave("plots/map-all-hki.png", p2_hki, width = 20, height = 15, units = "cm", dpi = 600)
+ggsave("plots/map-all-helsinki.png", p2_h, width = 20, height = 15, units = "cm", dpi = 600)
+ggsave("plots/map-all-helsinki2.png", p2_h2, width = 20, height = 15, units = "cm", dpi = 600)
+ggsave("plots/map-all-helsinki-espoo.png", p2_he, width = 20, height = 15, units = "cm", dpi = 600)
+ggsave("plots/map-all-helsinki-espoo-vantaa.png", p2_hev, width = 20, height = 15, units = "cm", dpi = 600)
+ggsave("plots/map-all-uusimaa.png", p2, width = 20, height = 15, units = "cm", dpi = 600)
+
 
 print('Plot elevation profiles')
 p3 <- plot_elevations(data)
